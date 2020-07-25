5c13dd55cd7021cc3436ffbd7b13cc4f50d135ee
ssk0206 <coccinelle.sasa@gmail.com>
2020-07-25 11:56:47 +0900

    make migration

---
+++ database/migrations/2020_07_24_124836_create_columns_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateColumnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('columns', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('component_id')->comment('コンポーネントID');
+            $table->text('content')->comment('コンテンツ');
+            $table->integer('order')->comment('順番');
+            $table->timestamps();
+
+            $table->foreign('component_id')->references('id')->on('components');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('columns');
+    }
+}
