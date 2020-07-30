c81e770f6aaef93deef3a30327e2f5cb53f567c3
ssk0206 <coccinelle.sasa@gmail.com>
2020-07-30 14:48:55 +0900

    モーダル表示

--- frontend/src/views/Edit.vue
+++ frontend/src/views/Edit.vue
@@ -4,14 +4,19 @@
     <h1>This is an edit page {{ $route.params.id }}</h1>
     <draggable v-model="components" handle=".handle">
       <v-card
-      v-for="component in components" :key="component.id"
-      class="mx-auto"
-      max-width="600"
-      outlined>
+        v-for="(component, index) in components" :key="component.id"
+        class="mx-auto"
+        max-width="600"
+        outlined
+      >
         <div class="handle" style="background-color:#eee;">
           <v-icon medium style="float:left;">mdi-drag</v-icon>
-          <v-icon color="grey lighten-1" style="float:right;">mdi-delete</v-icon>
-          <v-icon color="grey lighten-1" style="float:right;">mdi-pencil</v-icon>
+          <v-btn icon small style="float:right;" height="23" width="23" @click="deleteComponent(index)">
+            <v-icon color="grey lighten-1" style="float:right;" small>mdi-delete</v-icon>
+          </v-btn>
+          <div style="margin:0 5px 0 0;float:right;">
+            <editor></editor>
+          </div>
         </div>
         <div>{{ component.columns[0].content }}</div>
       </v-card>
@@ -24,6 +29,7 @@
 
 <script>
 import draggable from 'vuedraggable'
+import Editor from '../components/Editor.vue'
 
 function dumpObj(obj) {
   return JSON.stringify(obj, null, 2)
@@ -31,7 +37,8 @@ function dumpObj(obj) {
 
 export default {
   components: {
-    draggable
+    draggable,
+    Editor
   },
   data() {
     return {
@@ -59,7 +66,13 @@ export default {
             }
           ]
         }
-      );
+      )
+    },
+    deleteComponent: function (index) {
+      this.components.splice(index, 1)
+    },
+    openModal: function (componentId) {
+      return componentId
     }
   },
   created() {
