5a31df4ab594ad9aa24f8dbf933cd8479cb20dbb
ssk0206 <coccinelle.sasa@gmail.com>
2020-07-30 20:12:20 +0900

    tiptap導入

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="600px">
+  <v-row justify="center" style="max-height:1000px">
+    <v-dialog v-model="dialog" persistent max-width="1000px" max-height="1000px;">
       <template v-slot:activator="{ on, attrs }">
           <v-btn
             icon
@@ -15,49 +15,10 @@
           </v-btn>
       </template>
       <v-card>
-        <v-card-title>
-          <span class="headline">User Profile</span>
-        </v-card-title>
         <v-card-text>
           <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
+            <tiptap></tiptap>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -70,9 +31,13 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      dialog: false,
-    }),
-  }
+import Tiptap from './Tiptap'
+export default {
+  components: {
+    Tiptap
+  },
+  data: () => ({
+    dialog: false,
+  }),
+}
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import VueAxios from 'vue-axios'
 import vuetify from './plugins/vuetify';
+import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
+// don't forget to import CSS styles
+import 'tiptap-vuetify/dist/main.css'
+// Vuetify's CSS styles
+import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 const axiosBase = require('axios');
@@ -14,6 +19,15 @@ const axios = axiosBase.create({
   responseType: 'json'
 })
 Vue.use(VueAxios, axios)
+
+// const tiptapvuetify = new vuetify()
+Vue.use(TiptapVuetifyPlugin, {
+  // the next line is important! You need to provide the Vuetify Object to this place.
+  vuetify, // same as "vuetify: vuetify"
+  // optional, default to 'md' (default vuetify icons before v2.0.0)
+  iconsGroup: 'mdi'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
